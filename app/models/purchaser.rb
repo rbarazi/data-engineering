3154fca6a0e362dd780714fa7f3dfd6da078a049
rbarazi <rbarazi@gmail.com>
2012-05-14 07:36:40 -0400

    add purchaser associations and validations

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -1,3 +1,7 @@
 class Purchaser < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :orders
+
+  validates :name, presence: true
 end
--- spec/models/purchaser_spec.rb
+++ spec/models/purchaser_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Purchaser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:orders) }
+  it { should validate_presence_of(:name) }
 end
