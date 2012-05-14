3977f92756cd5909102ca4edd1b4e219ab5c4089
rbarazi <rbarazi@gmail.com>
2012-05-14 07:31:54 -0400

    add merchant associations and validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,8 @@
 class Merchant < ActiveRecord::Base
   attr_accessible :address, :name
+
+  has_many :items
+  has_many :orders, through: :items
+
+  validates :address, :name, presence: true
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Merchant do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:items) }
+  it { should have_many(:orders).through(:items) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:address) }
 end
