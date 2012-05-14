19643b60c2b002b69c21f04adc01f5848a95097a
rbarazi <rbarazi@gmail.com>
2012-05-14 07:39:26 -0400

    add order associations and validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ActiveRecord::Base
   attr_accessible :item_id, :purchaser_id, :quantity
+
+  belongs_to :item
+  belongs_to :purchaser
+
+  validates :quantity, presence: true
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Order do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:item) }
+  it { should belong_to(:purchaser) }
+  it { should validate_presence_of(:quantity) }
 end
